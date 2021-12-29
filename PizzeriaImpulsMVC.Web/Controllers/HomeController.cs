9fb306368dde6f0162069d646406e79a336d8bd2
sjakub96 <69714548+sjakub96@users.noreply.github.com>
2021-12-30 00:13:22 +0100

    Revert "Add SideBar Menu"
    This reverts commit e365181170f4cdf60d161974b6962865db613ed4.

--- PizzeriaImpulsMVC.Web/Controllers/HomeController.cs
+++ PizzeriaImpulsMVC.Web/Controllers/HomeController.cs
@@ -6,25 +6,22 @@ namespace PizzeriaImpulsMVC.Web.Controllers
 {
     public class HomeController : Controller
     {
-        /*
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-        */
 
         public IActionResult Index()
         {
             return View();
         }
-        
+
         public IActionResult Privacy()
         {
-            return PartialView();
+            return View();
         }
-        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
