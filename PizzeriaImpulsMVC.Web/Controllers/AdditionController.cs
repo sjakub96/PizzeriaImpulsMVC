2fdc11cf4ac54a7b72b3b20336c31cb8a7a8044d
sjakub96 <69714548+sjakub96@users.noreply.github.com>
2021-12-30 21:52:04 +0100

    Add controllers (addition & component)

---
+++ PizzeriaImpulsMVC.Web/Controllers/AdditionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzeriaImpulsMVC.Application.ViewModels.Addition;
+
+namespace PizzeriaImpulsMVC.Web.Controllers
+{
+    public class AdditionController : Controller
+    {
+        [HttpGet]
+        [Route("addition/all")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("addition/add")]
+        public IActionResult AddAddition()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddAddition(NewAdditionVm newAdditionVm)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteAddition(int additionId)
+        {
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
---
+++ PizzeriaImpulsMVC.Web/Controllers/ComponentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzeriaImpulsMVC.Application.ViewModels.Component;
+
+namespace PizzeriaImpulsMVC.Web.Controllers
+{
+    public class ComponentController : Controller
+    {
+        [HttpGet]
+        [Route("component/all")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("component/add")]
+        public IActionResult AddComponent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddComponent(NewComponentVm newComponentVm)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComponent(int componentId)
+        {
+            return RedirectToAction("Index");
+        }
+
+    }
+}
