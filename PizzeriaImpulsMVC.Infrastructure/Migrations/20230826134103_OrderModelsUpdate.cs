f276f7d6b41d9a1c7f9c0d47521820259763856d
sjakub96 <69714548+sjakub96@users.noreply.github.com>
2023-08-26 15:41:38 +0200

    Update order models - migration

---
+++ PizzeriaImpulsMVC.Infrastructure/Migrations/20230826134103_OrderModelsUpdate.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzeriaImpulsMVC.Infrastructure.Migrations
+{
+    public partial class OrderModelsUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Additions_AdditionId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Drinks_DrinkId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Pizzas_PizzaId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetails_AdditionId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetails_DrinkId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetails_PizzaId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "AdditionId",
+                table: "OrderDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "HombeNumber",
+                table: "Orders",
+                newName: "HomeNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "PizzaId",
+                table: "OrderDetails",
+                newName: "ProductId");
+
+            migrationBuilder.RenameColumn(
+                name: "DrinkId",
+                table: "OrderDetails",
+                newName: "ProductCount");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "OrderDetails",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "OrderDetails",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ProductSize",
+                table: "OrderDetails",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductType",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ProductSize",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ProductType",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "OrderDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "HomeNumber",
+                table: "Orders",
+                newName: "HombeNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "OrderDetails",
+                newName: "PizzaId");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductCount",
+                table: "OrderDetails",
+                newName: "DrinkId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AdditionId",
+                table: "OrderDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_AdditionId",
+                table: "OrderDetails",
+                column: "AdditionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_DrinkId",
+                table: "OrderDetails",
+                column: "DrinkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_PizzaId",
+                table: "OrderDetails",
+                column: "PizzaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Additions_AdditionId",
+                table: "OrderDetails",
+                column: "AdditionId",
+                principalTable: "Additions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Drinks_DrinkId",
+                table: "OrderDetails",
+                column: "DrinkId",
+                principalTable: "Drinks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Pizzas_PizzaId",
+                table: "OrderDetails",
+                column: "PizzaId",
+                principalTable: "Pizzas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
