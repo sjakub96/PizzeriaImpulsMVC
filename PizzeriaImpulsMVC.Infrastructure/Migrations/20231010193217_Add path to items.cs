0eb0c3a29d903b5165433238c2aad230b1722ddd
sjakub96 <69714548+sjakub96@users.noreply.github.com>
2023-10-10 21:44:50 +0200

    Working on images

---
+++ PizzeriaImpulsMVC.Infrastructure/Migrations/20231010193217_Add path to items.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzeriaImpulsMVC.Infrastructure.Migrations
+{
+    public partial class Addpathtoitems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImgPath",
+                table: "Pizzas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgPath",
+                table: "Drinks",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgPath",
+                table: "Components",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgPath",
+                table: "Additions",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgPath",
+                table: "Pizzas");
+
+            migrationBuilder.DropColumn(
+                name: "ImgPath",
+                table: "Drinks");
+
+            migrationBuilder.DropColumn(
+                name: "ImgPath",
+                table: "Components");
+
+            migrationBuilder.DropColumn(
+                name: "ImgPath",
+                table: "Additions");
+        }
+    }
+}
